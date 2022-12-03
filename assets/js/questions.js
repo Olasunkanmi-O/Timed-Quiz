259f10b795ce8b48482a53ffd164d556b1178e3f
Olasunkanmi <olasunkanmi.m.owolabi@gmail.com>
2022-12-03 22:40:40 +0000

    initial commit

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+
+
+var quesstion = [
+{
+    title: 'In which HTML element do we embed JavaScript?',
+    choices: ['<script>', '<Javascript>', '<src>', '<a>'],
+    answer: '<script>'
+},
+{
+    title: 'What does Event Delegation mean to you?',
+    choices: ['Delegating events around children of a DOM', 'Attaching event listener on the parent', 'Reducing event activities to the children', 'iterating events within objects of document'],
+    answer: ''
+},
+{
+    title: '',
+    choices: [''],
+    answer: ''
+},
+{
+    title: '',
+    choices: [''],
+    answer: ''
+},
+{
+    title: '',
+    choices: [''],
+    answer: ''
+},
+{
+    title: '',
+    choices: [''],
+    answer: ''
+}
+]
