5f8d527eb4c9504c63165cc503691d5281e9329c
Olasunkanmi <olasunkanmi.m.owolabi@gmail.com>
2022-12-03 23:07:36 +0000

    updated the questions js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,26 +9,27 @@ var quesstion = [
 {
     title: 'What does Event Delegation mean to you?',
     choices: ['Delegating events around children of a DOM', 'Attaching event listener on the parent', 'Reducing event activities to the children', 'iterating events within objects of document'],
-    answer: ''
+    answer: 'Attaching event listener on the parent'
 },
 {
-    title: '',
-    choices: [''],
-    answer: ''
+    title: 'What is the correct way to write a JavaScript array?',
+    choices: ['var colors = {"red","blue","yellow"}', 'var colors = ("red","blue","yellow")', 'var colors =["red", "blue", "yellow"]', 'var colors = [0:"red", 1:"blue", 2:"yellow"]'],
+    answer: 'var colors =["red", "blue", "yellow"]'
 },
 {
-    title: '',
-    choices: [''],
-    answer: ''
+    title: 'Where is the correct place to insert a JavaScript?',
+    choices: ['<Head>', 'anywhere', '<body>', '<h1>',],
+    answer: '<body>'
 },
 {
-    title: '',
-    choices: [''],
-    answer: ''
+    title: 'How do you write "Hello World" in an alert box?',
+    choices: ['confirm("Hello World")', 'alert("Hello World")', 'prompt("Hello World")', 'print("Hello World")'],
+    answer: 'alert("Hello World")'
 },
 {
-    title: '',
-    choices: [''],
-    answer: ''
+    title: 'How do you call a function named "Printer"?',
+    choices: ['printer', 'call printer', 'call function printer()', 'Printer()'],
+    answer: 'Printer()'
 }
+
 ]
